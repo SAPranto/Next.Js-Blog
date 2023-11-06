3ef765846d201956165d9b672cf5f77effbe3e75
Shihab Ahmed Pranto <Pranto@ZeroGrounds-Mac-mini.local>
2023-11-06 17:02:43 +0600

    Navbar Edited

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className=' bg-indigo-900 h-screen w-full bg[url(https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80)]'>
+
+    </div>
   )
 }
 
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,8 @@
-'use client';
+"use client";
+import Link from "next/link";
 import React, { useState } from "react";
 import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
+import { BiSearchAlt, BiCart, BiHeart, BiUser } from "react-icons/bi";
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -13,30 +15,51 @@ const Navbar = () => {
     <div className="bg-black text-white relative">
       <div className="main-container">
         <div className="navbar-wrapper flex justify-between items-center py-3">
-          <div>
-            <img src="./logo.webp" alt="logo" width={240} />
+          <div className="flex justify-start items-center gap-14">
+            <div>
+              <img src="./logo.webp" alt="logo" width={160} />
+            </div>
+            <ul
+              className={`md:flex gap-5 text-sm items-center ${
+                isNavOpen
+                  ? "flex flex-col gap-10 absolute right-0 top-0 bg-slate-500 text-center p-12 md:hidden"
+                  : "hidden"
+              }`}
+            >
+              <li className="cursor-pointer">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="cursor-pointer">
+                <Link href="/about">About</Link>
+              </li>
+              <li className="cursor-pointer">
+                <Link href="/team">Team</Link>
+              </li>
+              <li className="cursor-pointer">
+                <Link href="/contact">Contact Us</Link>
+              </li>
+            </ul>
           </div>
-          <ul
-            className={`md:flex gap-5 items-center ${
-              isNavOpen
-                ? "flex flex-col gap-10 absolute right-0 top-0 bg-slate-500 text-center p-12 md:hidden"
-                : "hidden"
-            }`}
-          >
-            <li>Home</li>
-            <li>About</li>
-            <li>Team</li>
-            <li>Contact Us</li>
-            <li>
-              <button className="px-4 py-2 bg-blue-400 rounded hover:bg-blue-700 transition-colors duration-500">Click Me</button>
-            </li>
-          </ul>
-          <div className="md:hidden z-10">
-            {isNavOpen ? (
-              <GiCancel onClick={handleNavToggle} className=" text-red-400 text-xl" />
-            ) : (
-              <GiHamburgerMenu onClick={handleNavToggle} className=" text-xl"/>
-            )}
+          <div className="flex items-center gap-3">
+            <div className="flex justify-start gap-2 text-sm">
+              <BiSearchAlt></BiSearchAlt>
+              <BiCart></BiCart>
+              <BiHeart></BiHeart>
+              <BiUser></BiUser>
+            </div>
+            <div className="md:hidden z-10">
+              {isNavOpen ? (
+                <GiCancel
+                  onClick={handleNavToggle}
+                  className=" text-red-400 text-xl cursor-pointer"
+                />
+              ) : (
+                <GiHamburgerMenu
+                  onClick={handleNavToggle}
+                  className=" text-xl cursor-pointer"
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
