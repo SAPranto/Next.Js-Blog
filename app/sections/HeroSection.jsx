2e44925d88a34eb5594ab734cfb6f916bbcbbddd
Shihab Ahmed Pranto <Pranto@ZeroGrounds-Mac-mini.local>
2023-12-01 17:28:13 +0600

    Navbar MegaMenu State Created

--- app/sections/HeroSection.jsx
+++ app/sections/HeroSection.jsx
@@ -4,8 +4,8 @@ const HeroSection = () => {
   return (
     <div className='hero'>
       <div className='flex flex-col h-full justify-end md:items-end'>
-        <div className='flex flex-col gap-4 md:gap-10 pb-10 md:pb-14 md:pe-10 z-10'>
-          <h1 className='text-center text-3xl md:text-5xl font-bold text-white'>Holiday 2023</h1>
+        <div className='flex flex-col gap-4 md:gap-6 pb-10 md:pb-20 md:pe-10 z-10'>
+          <h1 className='text-center text-3xl md:text-5xl font-bold text-white uppercase'>Holiday 2023</h1>
           <div className='flex justify-center'>
           <button className='hero-btn'>Shop Gifts</button>
           <button className='hero-btn border-s-2 border-black'>Shop Gifts</button>
